b01ccf6083cd20088b9bbbc57dfd39fb6f32fde8
Rahul Gaba <rahul_gaba@external.mckinsey.com>
2016-11-28 15:49:49 +0530

    comments removed

--- demo.js
+++ demo.js
@@ -1,18 +1,4 @@
 var app = angular.module('demo', ['seeMore']);
 
 app
-  .controller('main', [function() {
-    let $ctrl = this;
-  // $ctrl.filters = filters;
-  // $ctrl.selectedFilters = [];
-  // $ctrl.hoveredOn = '';
-  // $ctrl.filterHover = (filter) => {
-  //   $ctrl.hoveredOn = filter.name;
-  // };
-  // $ctrl.filterClick = (filter) => {
-  //   console.log('from click controller', filter);
-  // };
-  // $scope.$on('FACET_CHANGED', (evnt, selectedFilters) => {
-  //   $ctrl.selectedFilters = selectedFilters;
-  // })
-  }]);
+  .controller('main', [function() {}]);
