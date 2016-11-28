ec7744b2139747820d4477cdfe6dc9dfe195fb50
Rahul Gaba <rahul_gaba@external.mckinsey.com>
2016-11-28 15:49:36 +0530

    demo done

---
+++ demo.js
@@ -0,0 +1,18 @@
+var app = angular.module('demo', ['seeMore']);
+
+app
+  .controller('main', [function() {
+    let $ctrl = this;
+  // $ctrl.filters = filters;
+  // $ctrl.selectedFilters = [];
+  // $ctrl.hoveredOn = '';
+  // $ctrl.filterHover = (filter) => {
+  //   $ctrl.hoveredOn = filter.name;
+  // };
+  // $ctrl.filterClick = (filter) => {
+  //   console.log('from click controller', filter);
+  // };
+  // $scope.$on('FACET_CHANGED', (evnt, selectedFilters) => {
+  //   $ctrl.selectedFilters = selectedFilters;
+  // })
+  }]);
--- dist/limittextdirective.js
+++ dist/limittextdirective.js
@@ -6,22 +6,22 @@
  * @description
  * # limitTextDirective
  */
-angular.module('ttsaApp')
+angular.module('seeMore', [])
   .directive('limitTextDirective', function() {
     return {
       restrict: 'E',
-      transclude: true,
       scope: {
         limitChar: '=',
         textToLimit: '='
       },
-      template: '<div class="limited-text-container" ng-transclude>{{limitedText}}\
+      template: '<div class="limited-text-container" >{{limitedText}}...\
                 <span ng-if="showSeeMore" class="see-more pointer">See More</span>\
                 <div class="hover-text">{{textToLimit}}</div>',
       link: function postLink(scope, element, attrs) {
         scope.showHover = false;
         scope.showSeeMore = scope.textToLimit.length >= scope.limitChar;
         scope.limitedText = scope.showSeeMore ? scope.textToLimit.slice(0, scope.limitChar) : scope.textToLimit;
+        console.log('log:', 'scope.textToLimit', scope);
       }
     };
   });
